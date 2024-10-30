23149b444069c3d5dd4d4c456cb730198d5c991a
Owen Merrill <91266947+TheChozoLord@users.noreply.github.com>
2024-10-30 10:49:22 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package linkedLists;
 
 public class Main {
     public static void main(String[] args) {
-        FlightReservation Delta = new FlightReservation(Jared Barnes, );
+        FlightReservation Delta = new FlightReservation(Jared Barnes, 16C, 5923);
+        FlightReservation American = new FlightReservation(Mal, 1d, 9793)
     }
 }
